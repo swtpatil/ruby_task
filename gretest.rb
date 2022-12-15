4e74d3979f39d3aaf6cf840a5b5f1de2ee4475b6
swtpatil <swpatil@bestpeers.com>
2022-12-15 19:13:04 +0530

    Add existing file

---
+++ gretest.rb
@@ -0,0 +1,10 @@
+puts "Enter two numbers"
+num1 = gets
+num2 = gets
+if num1 == num2
+puts "#{num1} is equal to #{num2}"
+elsif num1 > num2 
+puts "#{num1} is greater than #{num2}"
+else 
+puts "#{num2} is greater than #{num1}"
+end
